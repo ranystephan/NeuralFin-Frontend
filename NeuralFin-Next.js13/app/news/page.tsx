87e3364b2c5680eac563b63eea6dadc5d45a292e
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-03-09 20:29:26 +0200

    news page config and component init

--- NeuralFin-Next.js13/app/news/page.tsx
+++ NeuralFin-Next.js13/app/news/page.tsx
@@ -1,19 +1,26 @@
+import StockForm from '@/components/StockForm'
 import Header_News from '../../components/Header_News'
 
 
 
 
 
+
 const styles = {
-  wrapper: "w-screen h-screen flex flex-col",
+  wrapper: "static w-screen h-screen flex flex-col",
 
 
 }
 
 export default function Home() {
   return (
-    <div className={styles.wrapper}>
-      <Header_News />
+    <div>
+      <div className={styles.wrapper}>
+        <Header_News />
+        <div className="absolute left-0 top-20 bottom-0 w-72 border-r border-gray-200">
+          <StockForm />
+        </div>
+      </div>
     </div>
 
 
---
+++ NeuralFin-Next.js13/components/StockForm.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+
+
+type OverviewData = {
+  Name: string;
+  Exchange: string;
+  Description: string;
+};
+
+
+
+const styles = {
+  wrapper: "overflow-hidden bg-white shadow sm:rounded-lg",
+  companyName: "text-2xl font-bold mb-4",
+  description: "text-sm text-gray-400",
+
+}
+
+function StockForm() {
+  const [symbol, setSymbol] = useState('');
+  const [overviewData, setOverviewData] = useState(null as OverviewData | null);
+  const apiKey = process.env.ALPHA_VANTAGE_API_KEY;
+  
+
+  async function fetchOverviewData() {
+    try {
+      const response = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${apiKey}`);
+      const data = await response.json();
+      setOverviewData(data);
+    } catch (error) {
+      console.error('Error fetching overview data:', error);
+    }
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    fetchOverviewData();
+  }
+
+  return (
+    <div>
+      <div className={styles.wrapper}>
+        <div className="px-4 sm:px-6">
+{/*           <h3 className="text-base font-semibold leading-6 text-gray-900">Stock Details</h3>
+          <p className="mt-1 max-w-2xl text-sm text-gray-500 mb-2">Get an overview of the stock you wish to input</p> */}
+          <div>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor='stock-ticker' />
+              <div className="mt-2 flex mb-4">
+                  <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 px-3 text-gray-500 sm:text-sm">
+                    Symbol
+                  </span>
+                  <input
+                    type="text"
+                    value={symbol} 
+                    onChange={event => setSymbol(event.target.value)}
+                    name="stock-ticker"
+                    id="stock-ticker"
+                    className="block w-full p-4 flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green sm:text-sm sm:leading-6"
+                    placeholder="AAPL"
+                  />
+                </div>
+              {overviewData ? (
+                <div>
+                  <div className={styles.companyName}>{overviewData.Name}</div>
+                  <div className={styles.description}>{overviewData.Description}</div>
+                </div>
+              ) : (
+                <div>Loading...</div>
+              )}
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StockForm;
