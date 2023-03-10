e793cf82d9e41554bc3290b3ff7dbee6ffa23236
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-03-10 04:14:59 +0200

    home page config

--- NeuralFin-Next.js13/sections/About.jsx
+++ NeuralFin-Next.js13/sections/About.jsx
@@ -1,7 +1,17 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer} from '../utils/motion';
+import '../utils/motion';
+
+
+
 const About = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className='gradient-02 z-0' />
     About section
   </section>
 );
