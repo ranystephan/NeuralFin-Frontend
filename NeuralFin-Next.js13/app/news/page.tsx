47fb6ed99024214b2e332955f30c9706a85827b6
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-03-11 20:55:41 +0200

    better phone representation

--- NeuralFin-Next.js13/app/news/page.tsx
+++ NeuralFin-Next.js13/app/news/page.tsx
@@ -1,28 +1,44 @@
+'use client'
+
 import StockForm from '@/components/StockForm'
 import Header_News from '../../components/Header_News'
-
-
-
-
-
+import Header_News_Phones from '../../components/Header_News_Phones'
+/* import MobileMenu from '@/components/MobileMenu' */
+import { useState, useEffect } from 'react';
 
 const styles = {
   wrapper: "static w-screen h-screen flex flex-col",
-
-
 }
 
 export default function Home() {
-  return (
-    <div>
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Change the breakpoint to match your needs
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  if (isMobile) {
+    return (
       <div className={styles.wrapper}>
-        <Header_News />
-        <div className="absolute left-0 top-20 bottom-0 w-72 border-r border-gray-200">
-          <StockForm />
-        </div>
+        <Header_News_Phones />
+        {/* <MobileMenu /> */}
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <Header_News />
+      <div className="absolute left-0 top-20 bottom-0 w-72 border-r border-gray-200">
+        <StockForm />
       </div>
     </div>
-
-
-  )
+  );
 }
--- NeuralFin-Next.js13/components/Header_News_Phones.tsx
+++ NeuralFin-Next.js13/components/Header_News_Phones.tsx
@@ -7,9 +7,17 @@ const styles = {
   rightHeader: 'flex items-center justify-end text-white gap-8 mr-5 mt-3 text-2xl',
   logoname: 'cursor-pointer font-bold hover:text-purple-700 duration-300 text-black',
   menuItem: 'cursor-pointer font-bold hover:text-purple-700 duration-300',
+  searchForm: 'relative',
+  searchInput: ' h-8 px-3 pr-8 text-sm w-28 focus:outline-none border-none placeholder-white placeholder:text-2xl',
+  searchButton: 'absolute bg-green-500 hover:bg-green-700 h-8 right-0 rounded-full text-white w-8'
 }
 
-const Header_News = () => {
+const Header_News_Phones = () => {
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // handle search submit here
+  }
+
   return (
     <div className='page-container'>
       <div className='left-half'>
@@ -22,9 +30,16 @@ const Header_News = () => {
       </div>
 
       <div className='right-half'>
+        <div className={styles.rightHeader}>
+          <form className={styles.searchForm} onSubmit={handleSearchSubmit}>
+            <input type="text" placeholder="search" className={styles.searchInput} style={{ backgroundColor: 'transparent'}} />
+  {/*           <button type="submit" className={styles.searchButton}>🔍</button>*/}        
+          </form>
+        </div>
+
       </div>
     </div>
   )
 }
 
-export default Header_News
+export default Header_News_Phones;
