7955fe0f5b360727bd8bb1f80e531ee722fbaf4f
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-03-11 02:51:41 +0200

    removed wavegrid

--- NeuralFin-Next.js13/app/page.tsx
+++ NeuralFin-Next.js13/app/page.tsx
@@ -4,7 +4,6 @@
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 import { ThemeProvider } from 'next-themes';
-import WaveGrid from '../components/WaveGrid';
 
 const Page = () => (
   <ThemeProvider attribute="class">
