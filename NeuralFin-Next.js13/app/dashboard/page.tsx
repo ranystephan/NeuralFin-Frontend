75e65516e3223c872b2c2af6cebb34b4ecec8c36
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-04-28 08:30:58 +0300

    test5

--- NeuralFin-Next.js13/app/dashboard/page.tsx
+++ NeuralFin-Next.js13/app/dashboard/page.tsx
@@ -20,11 +20,10 @@ import chartImage from '@/public/chartImage.png'
 import Link from 'next/link'
 
 
-
 const stylesL = {
   wrapper: "w-screen h-screen flex flex-col bg-black",
-  mainContainer: ' w-5/6 h-full m-auto flex mt-16 rounded-3xl bg-[#252522]',
-  leftMain: 'flex flex-col w-3/4 h-full p-6 overflow-y-scroll',
+  mainContainer: ' w-5/6 h-full m-auto flex mt-16 rounded-3xl bg-[#252522] z-100',
+  leftMain: 'flex flex-col w-3/4 h-full p-6 overflow-y-scroll z-100',
   portfolioAmountContainer: 'flex flex-col',
   portfolioAmount: 'text-white text-4xl',
   portfolioPercent: 'text-white font-bold text-sm',
@@ -44,10 +43,16 @@ const stylesL = {
   stocksContainer: 'flex flex-row flex-1 bg-purple-500 rounded-lg px-5 justify-between',
   ItemTitle: 'flex-1 font-bold',
   moreOptions: 'curesor-pointer text-xl',
+  container: " h-screen w-screen overflow-hidden bg-purple-100 z[-10]",
+  shape1: "absolute top-0 -left-4 w-96 h-80 bg-purple-200 rounded-full mix-blend-multiply filter blur-3xl animate-blob1 z[-100] ",
+  shape2: "absolute top-0 -right-4 w-96 h-96 bg-green-200 rounded-full mix-blend-multiply filter blur-3xl animate-blob2 z[-100]",
+  shape3: "absolute bottom-0 -right-4 w-96 h-96 bg-purple-200 rounded-full mix-blend-multiply filter blur-3xl animate-blob3 z[-100]",
+  shape4: "absolute bottom-0 -left-4 w-96 h-96 bg-green-200 rounded-3xl mix-blend-multiply filter blur-3xl animate-blob4 z[-100]",
+  blobContainer: "flex flex-col items-center justify-center w-full z-[-100]",
 
 }
 
-export default function Home() {
+export default function Dashboard() {
   const [popularTopics, setTopics] = useState([
     "Technology",
     "Top Movies",
@@ -63,143 +68,150 @@ export default function Home() {
 
 
   return (
-    
-    <div className={stylesL.wrapper}>
-      <DashboardHeader />
-      <div className={stylesL.mainContainer}>
-        <div className={stylesL.leftMain}>
-          <div className={stylesL.portfolioAmountContainer}>
-            <div className={stylesL.portfolioAmount}>$107,486.21</div>
-            <div className={stylesL.portfolioPercent}>
-              +12.420(+1.69%)
-              <span className={stylesL.pastHour}>Past Hour</span>
-            </div>
-          </div>
-          <div>
-            <div className={stylesL.chartContainer}>
-              {/* <PortfolioChart */}
-              <img className='rounded-2xl' src={chartImage.src} alt="charrt" width='700' height='300' />
-            </div>
-          </div>
-          <div className={stylesL.buyingPowerContainer}>
-            <div className={stylesL.buyingPowerTitle}>Buying Power</div>
-            <div className={stylesL.buyingPowerAmount}>$12,534.21</div>
-          </div>
-          <div className={stylesL.buyingPowerContainer}>
-            <Link href={"/info"}>
-              <div className={stylesL.buyingPowerTitle}>Value at Risk</div>
-            </Link>
-            <div className={stylesL.buyingPowerAmount}>2.06%</div>
-          </div>
-          <div className={stylesL.buyingPowerContainer}>
-            <div className={stylesL.buyingPowerTitle}>Buying Power</div>
-            <div className={stylesL.buyingPowerAmount}>$12,534.21</div>
-          </div>
-          <div className={stylesL.notice}>
-            <div className="newsfeed__popularlists__section">
-              <div className="newsfeed__popularlists__intro">
-                <div className='font-bold text-white text-xl'>Popular lists</div>
-                <p>Show More</p>
-              </div>
-              <div className="newsfeed_popularlists_badges">
-                {popularTopics.map((topic) => (
-                  <Chip 
-                    className="topic__badge"
-                    variant="outlined"
-                    label={topic}
-                    avatar={<Avatar
-                      src={`https://avatars.dicebear.com/api/human/${topic}.svg`}
-                    />} 
-                  />
-                ))}
+    <div>
+      <div className={stylesL.blobContainer}>
+        <div className={stylesL.shape2}></div>
+        <div className={stylesL.shape3}></div>
+        <div className={stylesL.shape1}></div>
+        <div className={stylesL.shape4}></div>
+      </div>
+      <div className={stylesL.wrapper}>
+        <DashboardHeader />
+        <div className={stylesL.mainContainer}>
+          <div className={stylesL.leftMain}>
+            <div className={stylesL.portfolioAmountContainer}>
+              <div className={stylesL.portfolioAmount}>$107,486.21</div>
+              <div className={stylesL.portfolioPercent}>
+                +12.420(+1.69%)
+                <span className={stylesL.pastHour}>Past Hour</span>
               </div>
             </div>
+            <div>
+              <div className={stylesL.chartContainer}>
+                {/* <PortfolioChart */}
+                <img className='rounded-2xl' src={chartImage.src} alt="charrt" width='700' height='300' />
+              </div>
+            </div>
+            <div className={stylesL.buyingPowerContainer}>
+              <div className={stylesL.buyingPowerTitle}>Buying Power</div>
+              <div className={stylesL.buyingPowerAmount}>$12,534.21</div>
+            </div>
+            <div className={stylesL.buyingPowerContainer}>
+              <Link href={"/info"}>
+                <div className={stylesL.buyingPowerTitle}>Value at Risk</div>
+              </Link>
+              <div className={stylesL.buyingPowerAmount}>2.06%</div>
+            </div>
+            <div className={stylesL.buyingPowerContainer}>
+              <div className={stylesL.buyingPowerTitle}>Buying Power</div>
+              <div className={stylesL.buyingPowerAmount}>$12,534.21</div>
+            </div>
+            <div className={stylesL.notice}>
+              <div className="newsfeed__popularlists__section">
+                <div className="newsfeed__popularlists__intro">
+                  <div className='font-bold text-white text-xl'>Popular lists</div>
+                  <p>Show More</p>
+                </div>
+                <div className="newsfeed_popularlists_badges">
+                  {popularTopics.map((topic) => (
+                    <Chip 
+                      className="topic__badge"
+                      variant="outlined"
+                      label={topic}
+                      avatar={<Avatar
+                        src={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+                      />} 
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+            {/* <Notice/> */}
           </div>
-          {/* <Notice/> */}
-        </div>
-        <div className={stylesL.rightMain}>
-          <div className={stylesL.rightMainItem}>
-            <div className={stylesL.ItemTitle}> Performing </div>
+          <div className={stylesL.rightMain}>
+            <div className={stylesL.rightMainItem}>
+              <div className={stylesL.ItemTitle}> Performing </div>
 
-            <BiDotsHorizontalRounded className={stylesL.moreOptions} />
+              <BiDotsHorizontalRounded className={stylesL.moreOptions} />
+              
+            </div>
+            {/* Map through stocks and for every stock make an Asset component */}
+            {/* <Asset /> */}
+
+            <div className={stylesL.rightMainItemStocks}>
+              <div className={stylesL.ItemTitle}>
+                <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-44 bg-opacity-30 hover:bg-opacity-60'>
+                  <div>AAPL</div>
+                  <div>32.5%</div>
+                </div>
+              </div>
+            </div>
+            <div className={stylesL.rightMainItemStocks}>
+              <div className={stylesL.ItemTitle}>
+                <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-40 bg-opacity-30 hover:bg-opacity-60'>
+                  <div>TSLA</div>
+                  <div>27.2%</div>
+                </div>
+              </div>
+            </div>
+            <div className={stylesL.rightMainItemStocks}>
+              <div className={stylesL.ItemTitle}>
+                <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-52 bg-opacity-30 hover:bg-opacity-60'>
+                  <div>NYSE</div>
+                  <div>11.7%</div>
+                </div>
+              </div>
+            </div>
+            <div className={stylesL.rightMainItemStocks}>
+              <div className={stylesL.ItemTitle}>
+                <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-48 bg-opacity-30 hover:bg-opacity-60'>
+                  <div>BABA</div>
+                  <div>30.5%</div>
+                </div>
+              </div>
+            </div>
+            <div className={stylesL.rightMainItemStocks}>
+              <div className={stylesL.ItemTitle}>
+                <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-56 bg-opacity-30 hover:bg-opacity-60'>
+                  <div>MSFT</div>
+                  <div>43.4%</div>
+                </div>
+              </div>
+            </div>
+            <div className={stylesL.rightMainItemStocks}>
+              <div className={stylesL.ItemTitle}>
+                <div className='flex flex-row flex-1 bg-purple-500 items-center rounded-lg h-7 px-5 justify-between w-44 bg-opacity-30 hover:bg-opacity-60'>
+                  <div>TWTR</div>
+                  <div>69%</div>
+                </div>
+              </div>
+            </div>
+            <div className={stylesL.rightMainItemStocks}>
+              <div className={stylesL.ItemTitle}>
+                <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-40 bg-opacity-30 hover:bg-opacity-60'>
+                  <div>NIO</div>
+                  <div>21.1%</div>
+                </div>
+              </div>
+            </div>
+            <div className={stylesL.rightMainItemStocks}>
+              <div className={stylesL.ItemTitle}>
+                <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-36 bg-opacity-30 hover:bg-opacity-60'>
+                  <div>ADA</div>
+                  <div>11.1%</div>
+                </div>
+              </div>
+            </div>
+            <div className={stylesL.rightMainItemStocks}>
+              <div className={stylesL.ItemTitle}>
+                <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-48 bg-opacity-30 hover:bg-opacity-60'>
+                  <div>NDAQ</div>
+                  <div>28.1%</div>
+                </div>
+              </div>
+            </div>
             
           </div>
-          {/* Map through stocks and for every stock make an Asset component */}
-          {/* <Asset /> */}
-
-          <div className={stylesL.rightMainItemStocks}>
-            <div className={stylesL.ItemTitle}>
-              <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-44 bg-opacity-30 hover:bg-opacity-60'>
-                <div>AAPL</div>
-                <div>32.5%</div>
-              </div>
-            </div>
-          </div>
-          <div className={stylesL.rightMainItemStocks}>
-            <div className={stylesL.ItemTitle}>
-              <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-40 bg-opacity-30 hover:bg-opacity-60'>
-                <div>TSLA</div>
-                <div>27.2%</div>
-              </div>
-            </div>
-          </div>
-          <div className={stylesL.rightMainItemStocks}>
-            <div className={stylesL.ItemTitle}>
-              <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-52 bg-opacity-30 hover:bg-opacity-60'>
-                <div>NYSE</div>
-                <div>11.7%</div>
-              </div>
-            </div>
-          </div>
-          <div className={stylesL.rightMainItemStocks}>
-            <div className={stylesL.ItemTitle}>
-              <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-48 bg-opacity-30 hover:bg-opacity-60'>
-                <div>BABA</div>
-                <div>30.5%</div>
-              </div>
-            </div>
-          </div>
-          <div className={stylesL.rightMainItemStocks}>
-            <div className={stylesL.ItemTitle}>
-              <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-56 bg-opacity-30 hover:bg-opacity-60'>
-                <div>MSFT</div>
-                <div>43.4%</div>
-              </div>
-            </div>
-          </div>
-          <div className={stylesL.rightMainItemStocks}>
-            <div className={stylesL.ItemTitle}>
-              <div className='flex flex-row flex-1 bg-purple-500 items-center rounded-lg h-7 px-5 justify-between w-44 bg-opacity-30 hover:bg-opacity-60'>
-                <div>TWTR</div>
-                <div>69%</div>
-              </div>
-            </div>
-          </div>
-          <div className={stylesL.rightMainItemStocks}>
-            <div className={stylesL.ItemTitle}>
-              <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-40 bg-opacity-30 hover:bg-opacity-60'>
-                <div>NIO</div>
-                <div>21.1%</div>
-              </div>
-            </div>
-          </div>
-          <div className={stylesL.rightMainItemStocks}>
-            <div className={stylesL.ItemTitle}>
-              <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-36 bg-opacity-30 hover:bg-opacity-60'>
-                <div>ADA</div>
-                <div>11.1%</div>
-              </div>
-            </div>
-          </div>
-          <div className={stylesL.rightMainItemStocks}>
-            <div className={stylesL.ItemTitle}>
-              <div className='flex flex-row flex-1 bg-orange-500 items-center rounded-lg h-7 px-5 justify-between w-48 bg-opacity-30 hover:bg-opacity-60'>
-                <div>NDAQ</div>
-                <div>28.1%</div>
-              </div>
-            </div>
-          </div>
-          
         </div>
       </div>
     </div>
@@ -207,4 +219,7 @@ export default function Home() {
   )
   
 }
+
+
+
 
