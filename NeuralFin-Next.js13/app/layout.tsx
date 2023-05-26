ce9a150b06258de64d608176f37095cd2e2242c7
ranystephan <ranystephan@gmail.com>
2023-05-26 13:12:38 +0300

    test

--- NeuralFin-Next.js13/app/layout.tsx
+++ NeuralFin-Next.js13/app/layout.tsx
@@ -1,26 +1,17 @@
-import { AuthProvider } from '@/contexts/AuthContext';
-import Head from 'next/head';
+import { AuthContext, AuthProvider } from '@/contexts/AuthContext';
 import '@/styles/globals.css';
 
 
-interface RootLayoutProps {
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode;
-}
-
-
-
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+}) {
   return (
     <AuthProvider>
       <html lang="en">
-        <Head>
-        </Head>
-        <body >
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
     </AuthProvider>
   );
-};
-
-export default RootLayout;
+}
--- NeuralFin-Next.js13/next.config.js
+++ NeuralFin-Next.js13/next.config.js
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
-
+// next.config.js
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: 'https://neuralfin-docs-production.up.railway.app/',
+      },
+    ];
+  },
+};
 
-export default nextConfig
+module.exports = nextConfig;
