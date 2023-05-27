444be2c4791b008ab4f3b1da8d85b954c32e6834
ranystephan <63688460+ranystephan@users.noreply.github.com>
2023-05-27 20:11:49 +0300

    some changes + docs implementation start

--- NeuralFin-Next.js13/components/UserNavbar.tsx
+++ NeuralFin-Next.js13/components/UserNavbar.tsx
@@ -72,6 +72,11 @@ const dropdownVariants = {
                 dashboard
               </div>
             </Link>
+            <Link href={"/docs"}>
+              <div className="font-extrabold sm:text-2xl text-3xl leading-8 cursor-pointer hover:text-purple-700 duration-300">
+                documentation
+              </div>
+            </Link>
           </motion.div>
     </div>
         <div className="font-extrabold text-4xl leading-8 cursor-pointer hover:text-purple-700 duration-300">
